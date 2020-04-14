d8f3fee60d8c9946cfaef615f7fff078803fd214
Asad <asad271084@gmail.com>
2020-04-14 12:26:42 +0600

    added a function in hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,5 @@
-console.log('Hello friends I am here')
+console.log('Hello friends I am here <br />')
+function asad(){
+	console.log("I have created my first github account and submit the file on the repojitory")
+}
+asad()
