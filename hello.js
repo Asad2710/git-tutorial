cc56902bbcbb4b23ae48bb1cdede86d2b02f09a2
Asad <asad271084@gmail.com>
2020-04-17 15:45:51 +0600

    I have added two new file and edit js file

--- hello.js
+++ hello.js
@@ -1,7 +1,12 @@
 console.log('Hello friends I am here <br />')
-function asad(){
-	console.log("I have created my first github account and submit the file on the repojitory")
+
+function asad() {
+    console.log("I have created my first github account and submit the file on the repojitory")
 }
 asad()
 var a = -1
-alert(a)
+    //alert(a)
+function surovy() {
+    console.log("Hiam surovy. What about you")
+}
+surovy();
